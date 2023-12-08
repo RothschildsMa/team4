485fa46680dc1c8493d3e1124b3a5995938333cb
RothschildsMa <unityidma@gmail.com>
2023-12-08 16:54:35 +0900

    a

--- team4/src/main/java/com/ntsed/api/controller/TestController.java
+++ team4/src/main/java/com/ntsed/api/controller/TestController.java
@@ -1,5 +1,13 @@
 package com.ntsed.api.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * author: aさん
+ * date:2023/12/08
+ * 
+*/
+@RestController
 public class TestController {
 
 }
