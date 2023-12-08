414966c4742670d7e1b39e37c484b01f17dc6e3b
RothschildsMa <unityidma@gmail.com>
2023-12-08 16:14:31 +0900

    first commit

---
+++ team4/src/main/java/com/ntsed/api/Team4Application.java
@@ -0,0 +1,13 @@
+package com.ntsed.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team4Application.class, args);
+	}
+
+}
---
+++ team4/src/test/java/com/ntsed/api/Team4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntsed.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
