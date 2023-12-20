007a4f5b9b5b23376d899578a0d734d2eb858ed9
SherryXTX <SherryXTX@users.noreply.github.com>
2023-12-20 16:45:58 +0900

    update

--- team4/src/main/java/com/ntsed/api/dto/UserAddRequest.java
+++ team4/src/main/java/com/ntsed/api/dto/UserAddRequest.java
@@ -3,8 +3,13 @@ package com.ntsed.api.dto;
 import java.io.Serializable;
 import java.sql.Date;
 
-import jakarta.persistence.Column;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -17,41 +22,39 @@ import lombok.Data;
 
 @Data
 public class UserAddRequest implements Serializable {
-   
-private Long employeeId;
-	
-    @NotEmpty(message = "氏名を入力してください")
-    @Size(max = 20, message = "氏名は20桁以内で入力してください")
-    private String employeeName;
-   
-    @NotEmpty(message = "所属を選択してください。")
-    private String deptId;
-    
-    @NotEmpty(message = "性別を選択してください。")
-    private String genderId;
-    
-  @Size(max = 11, min=10,message = "正しい電話番号を入力してください。")
-    private String employeeTel;
-    
-    private Integer delFlg;
-    
 
-    private Date entryDate;
-    
+	private Long employeeId;
 
-    @Column(length = 2)
-    private Integer employeeAge;
-    
-    @Pattern(regexp = "^([\\w])+([\\w\\._-])*\\@([\\w])+([\\w\\._-])*\\.([a-zA-Z])+$",message ="正しいメールアドレスはを入力してください。")
-    private String employeeMail;
-    
-    private String createUser;
-    
-    private Date createDate;
-    
-    private String loginUser;
-    
-    
-   
-  
+	@NotEmpty(message = "氏名を入力してください")
+	@Size(max = 20, message = "氏名は20桁以内で入力してください")
+	private String employeeName;
+
+	@NotEmpty(message = "所属を選択してください。")
+	private String deptId;
+
+	@NotEmpty(message = "性別を選択してください。")
+	private String genderId;
+
+	@Pattern(regexp = "^$|.{10,11}", message = "正しい電話番号を入力してください。")
+	private String employeeTel;
+
+	private Integer delFlg;
+
+	@Past
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date entryDate;
+
+	@Min(value = 10, message = "年齢は2桁を入力してください。")
+	@Max(value = 99, message = "年齢は2桁を入力してください。")
+	private Integer employeeAge;
+
+	@Email(message = "正しいメールアドレスはを入力してください。")
+	private String employeeMail;
+
+	private String createUser;
+
+	private Date createDate;
+
+	private String loginUser;
+
 }
